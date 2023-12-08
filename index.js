4f8320a3f6e64597f0d36c7277dc84f02cea1690
Phat Truong <thanhphat.994@gmail.com>
2023-12-08 22:05:11 +0700

    Init

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
 
 require('./routes/userRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  const path = require('path');
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   const path = require('path');
+//   app.get('*', (req,res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
