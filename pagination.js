e88a2352155a1043bccde27d5ce5b1419cac3cf6
Thanh Nguyen <42597660+thanhnguyen-agilityio@users.noreply.github.com>
2024-01-22 12:41:06 +0700

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -5,7 +5,7 @@
  * @returns 
  */
 export const getPagination = (page, size) => {
-  const limit = size ? +size : 3;
+  const limit = size ? size : 3;
   const from = page ? page * limit : 0;
   const to = page ? from + size : size;
 
