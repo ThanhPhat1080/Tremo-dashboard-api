1b293b5529fd27729b0679cca285dee18a701676
Thanh Nguyen <42597660+thanhnguyen-agilityio@users.noreply.github.com>
2024-03-19 10:32:35 +0700

    Get and set user pin code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -55,23 +55,48 @@ const userController = ({ app, supabase }) => {
     return res.status(201).send(createUserRes.data);
   });
 
-  // app.put(`/api/user`, async (req, res) => {
-  //   const { name, lastName, id } = req.body;
+  app.get(`/api/users/:userId`, async (req, res) => {
+    const userId = req.params.userId;
 
-  //   db.get('users')
-  //     .find({ id })
-  //     .assign({ name, lastName })
-  //     .write();
+    if (!userId) {
+      res.status(400).send("Missing user Id");
+    }
 
-  //   const user = db.get('users')
-  //     .find({ id })
-  //     .value();
 
-  //   return res.status(202).send({
-  //     error: false,
-  //     user
-  //   });
-  // });
+    const { data: users, error } = await supabase.from('users')
+      .select('id,createdAt,name,email,pinCode').eq('id', parseInt(userId));
+
+    if (error) {
+      res.status(500).send(error);
+    }
+
+    return res.status(200).send(users.length ? users[0] : {});
+  });
+
+  app.patch(`/api/users/:userId`, async (req, res) => {
+    const userId = req.params.userId;
+    const payload = req.body;
+    const { pinCode } = payload;
+
+    if (pinCode && payload.pinCode.toString().length < 6)
+    {
+      return res.status(400).send("PIN Code must have 6 digits.");
+    }
+
+    const data = {
+      pinCode: payload.pinCode
+    }
+    const { error } = await supabase
+      .from('users')
+      .update(data)
+      .eq('id', parseInt(userId))
+
+    if (error) {
+      res.status(500).send(error);
+    }
+
+    res.status(200).send({ message: 'Update success!' });
+  });
 
   // app.delete(`/api/user/:id`, async (req, res) => {
   //   const { id } = req.params;
