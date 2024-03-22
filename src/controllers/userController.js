0d89e158b2fb4ed20b44b3ba06db94268ed927ac
Thanh Nguyen <thanh.nguyen@asnet.com.vn>
2024-03-22 12:56:44 +0700

    Update user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,13 @@
 
 const userController = ({ app, supabase }) => {
 
+  /**
+   * @swagger
+   * /users:
+   *   get:
+   *     summary: Retrieve a list of users
+   *     description: Retrieve a list of users.
+  */
   app.get(`/api/users`, async (req, res) => {
 
     if (req.query.email) {
@@ -56,7 +63,7 @@ const userController = ({ app, supabase }) => {
   });
 
   app.get(`/api/users/:userId`, async (req, res) => {
-    const userId = req.params.userId;
+    const userId = req.params.userId ? parseInt(req.params.userId) : undefined;
 
     if (!userId) {
       res.status(400).send("Missing user Id");
@@ -74,28 +81,44 @@ const userController = ({ app, supabase }) => {
   });
 
   app.patch(`/api/users/:userId`, async (req, res) => {
-    const userId = req.params.userId;
-    const payload = req.body;
-    const { pinCode } = payload;
+    const userId = req.params.userId ? parseInt(req.params.userId) : undefined;
+    if (!userId) res.status(400).send({ message: 'Invalid user id.' });
 
-    if (pinCode && payload.pinCode.toString().length < 6)
-    {
-      return res.status(400).send("PIN Code must have 6 digits.");
+    // user not exist
+    const {
+      dataUser, getError
+    } = await supabase.from('users').select('id').eq('id', parseInt(userId))
+
+    if (!dataUser || getError) return res.status(400).send({ message: 'User not exist.' });
+
+    const { pinCode } = req.body;
+    const data = {};
+
+    // invalid pin code format
+    if (!pinCode || pinCode && pinCode.toString().length < 6) {
+      return res.status(400).send(
+        { message: 'PIN Code must have 6 digits.' }
+      );
     }
 
-    const data = {
-      pinCode: payload.pinCode
-    }
-    const { error } = await supabase
-      .from('users')
-      .update(data)
-      .eq('id', parseInt(userId))
-
-    if (error) {
-      res.status(500).send(error);
+    if (pinCode) {
+      data.pinCode = pinCode;
     }
 
-    res.status(200).send({ message: 'Update success!' });
+    if (!!Object.keys(data).length) {
+      const { error } = await supabase
+        .from('users')
+        .update(data)
+        .eq('id', parseInt(userId))
+
+      if (error) {
+        return res.status(500).send(error);
+      }
+
+      return res.status(200).send({ message: 'Update success!' });
+    }
+
+    return res.status(200).send({ message: 'Nothing to update!' });
   });
 
   // app.delete(`/api/user/:id`, async (req, res) => {
